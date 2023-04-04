0c243e1816666a59cb867bf334faa4ae9cfd509e
raju-miah <rajumiahpollob7@gmail.com>
2023-04-05 02:30:23 +0600

    added navbar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,37 @@ const loader = document.querySelector("[data-loader]");
 window.addEventListener("load", () =>{
     loader.classList.add("remove")
 })
+
+
+// Re-used elements function
+
+const addEventElements = function (elements, eventType, callBack){
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callBack)
+    }
+}
+
+// Nav tg Mobile
+
+const nav = document.querySelector("[data-nav]");
+
+const navTg = document.querySelectorAll("[data-nav-tg]");
+
+const overlay = document.querySelector("[data-overlay]");
+
+
+const tgNav = function () {
+    nav.classList.toggle("active")
+    overlay.classList.toggle("active")
+    document.body.classList.toggle("nav-active")
+}
+
+addEventElements(navTg, "click", tgNav)
+
+// For Header Sticky
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+    header.classList[this.window.scrollY > 100 ? "add" : "remove"]("active");
+})
